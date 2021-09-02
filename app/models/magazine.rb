872587a64c2a1455817906494ddcc9f1cce6282b
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-01 20:26:08 -0400

    Many2Many Done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,16 @@
 class Magazine < ActiveRecord::Base
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        reader_list = readers.pluck(:email)
+        reader_list.join(";")
+    end
+
+    def self.most_popular
+        all.max_by {|reader| reader.subscriptions.count}
+    end
+
+
   
 end
